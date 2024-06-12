de2283ff653116d8c063c70a56306954ba8881c2
HimanshuSingour <singourhimanshu3@gmail.com>
2024-06-12 22:16:48 +0530

    bug fix

--- src/main/java/com/oauth/implementation/dao/RoleRepository.java
+++ src/main/java/com/oauth/implementation/dao/RoleRepository.java
@@ -4,8 +4,6 @@ package com.oauth.implementation.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.oauth.implementation.model.Role;
-
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer>{
